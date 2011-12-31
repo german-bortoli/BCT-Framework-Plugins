958bdf71370b216417a908bcadb942343b93adfc
Marcus Povey <marcus@marcus-povey.co.uk>
2011-12-31 16:45:35 +0000

    Updated copyright information for 2012

--- jquery_ui/start.php
+++ jquery_ui/start.php
@@ -9,7 +9,7 @@
 	 * @package jquery_ui
 	 * @license The MIT License (see LICENCE.txt), other licenses available.
 	 * @author Marcus Povey <marcus@marcus-povey.co.uk>
-	 * @copyright Marcus Povey 2009-2011
+	 * @copyright Marcus Povey 2009-2012
 	 * @link http://www.marcus-povey.co.uk
 	 */
 
